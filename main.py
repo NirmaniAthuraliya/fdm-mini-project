366068fc4abdede1a321051708830e18f214dd9c
Nirmani Athuraliya <63805316+NirmaniAthuraliya@users.noreply.github.com>
2021-10-01 09:17:10 +0530

    Update main.py
    update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ hide_menu_style ="""
     </style>
 """
 
-# st.markdown(hide_menu_style, unsafe_allow_html=True)
+st.markdown(hide_menu_style, unsafe_allow_html=True)
 
 #load the model from disk
 import pickle
